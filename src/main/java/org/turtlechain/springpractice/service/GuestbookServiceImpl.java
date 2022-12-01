fe55c89de93d648d255f6ca4c6e95074e3ec1b4e
정보승 <bsjung@turtleship.io>
2022-12-01 17:04:06 +0900

    Feat: GuestbookService register 기능 추가

--- src/main/java/org/turtlechain/springpractice/service/GuestbookServiceImpl.java
+++ src/main/java/org/turtlechain/springpractice/service/GuestbookServiceImpl.java
@@ -1,13 +1,18 @@
 package org.turtlechain.springpractice.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.turtlechain.springpractice.dto.GuestbookDTO;
 import org.turtlechain.springpractice.entity.GuestBook;
+import org.turtlechain.springpractice.repository.GuestbookRepository;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookServiceImpl implements GuestbookService{
+
+    private final GuestbookRepository repository;
 
     @Override
     public Long register(GuestbookDTO dto) {
@@ -19,6 +24,9 @@ public class GuestbookServiceImpl implements GuestbookService{
 
         log.info(entity);
 
-        return null;
+        repository.save(entity);
+
+        return entity.getGno();
     }
+
 }
